7a60fff57583b7553e22d4b82dd2ab6bef828bfc
ndindiri <ndbut5@gmail.com>
2018-11-22 16:28:43 +0200

    datastore

---
+++ server/datastore/parcel.js
@@ -0,0 +1,26 @@
+const parcels = [
+    {
+    	id: 1,
+        userId: 2,
+        NameOfParcels: 'Parcel 1',
+        CountryFrom: 'Rwanda',
+        DeliveryCountry: 'Kenya',
+        Weigth: '2kg',
+        Heigh: '3cm',
+        Width: '5cm', 
+        Length: '4cm' 
+    },
+    {
+    	id: 2,
+     	userId: 1,
+		NameOfParcels: 'Parcel 2',
+		CountryFrom: 'Burundi',
+		DeliveryCountry: 'Tanzanie',
+		Weigth: '1.5kg',
+		Heigh: '2.5cm',
+		Width: '2.5cm',
+		Length: '2cm'
+	}
+];
+
+export default parcels;
