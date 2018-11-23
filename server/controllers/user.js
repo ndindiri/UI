1d8d142e81f8514e2b1dabe6013bd51c09c4a1ff
ndindiri <ndbut5@gmail.com>
2018-11-23 17:11:22 +0200

    user.js

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,5 @@
 import parcels from '../datastore/parcel';
-import user from '../datastore/user';
+import users from '../datastore/user';
 
 
 class User {
@@ -12,11 +12,10 @@ class User {
              if (!user) return res.status(404).send('The user with the the the given Id was not found');
            res.send(user);
     };
-     static getSingleUser = (req, res) =>{
-
-      const user = users.find(u => u.id === parseInt(req.params.id));
-      if (!user) return res.status(404).send('The parcel with the the the given Id was not found');
-       res.send(user);
+     static getParcelById = (req, res) =>{
+      const parcel = parcels.find(p => p.userId === parseInt(req.params.userId));
+  if (!parcel) return res.status(404).send('The parcel with the the the given Id was not found');
+  res.send(parcel);
 };
   }
 
