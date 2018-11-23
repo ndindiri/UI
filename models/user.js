c622e51990770c6273624d9d2b08d4dc571b5bbc
ndindiri <ndbut5@gmail.com>
2018-11-23 06:47:15 +0200

    models folder

---
+++ models/user.js
@@ -0,0 +1,30 @@
+class User {
+  constructor(userId, name, email) {
+    this.setUserId(userId);
+    this.setName(name);
+    this.setEmail(email);
+  }
+  setUserId(userId) {
+    this.userId = userId;
+  }
+
+  getUserId() {
+    return this.userId;
+  }
+
+  setName(name) {
+    this.name = name;
+  }
+
+  getName() {
+    return this.name;
+  }
+   setEmail(email) {
+    return this.email;
+  }
+  getEmail() {
+    return this.email;
+  }
+}
+
+export default User;
