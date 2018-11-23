de9b9f4dc1ffb222627e17f19eb3e730a1904f43
ndindiri <ndbut5@gmail.com>
2018-11-23 16:01:13 +0200

    users

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,37 +1,29 @@
 import parcels from '../datastore/parcel';
 import user from '../datastore/user';
 
-'use strict';
 
 class User {
-  constructor () {
+
+  static getAllUsers = (req, res) => {
+    res.status(200).json(users);
+  }
+   static getUserIdUser = (req, res) =>{
+             const user = users.find(u => u.id === parseInt(req.params.id));
+             if (!user) return res.status(404).send('The user with the the the given Id was not found');
+           res.send(user);
+    };
+     static getSingleUser = (req, res) =>{
+
+      const user = users.find(u => u.id === parseInt(req.params.id));
+      if (!user) return res.status(404).send('The parcel with the the the given Id was not found');
+       res.send(user);
+};
   }
 
-  getUser (req, res) {
-    user.findById(req.params.id).then(
-      (user) => {
-        res(user);
-      },
-      (err) => {
-        res(err);
-      }
-    )
-  }
-}
 
-module.exports = User;
+export default User;
 
 
 
 
 
-
-
-
-
-/*let UsersAndParcels = (p, ...arrs) => [ ...new Set( [].concat(...arrs).map(a => JSON.stringify(a)) ) ].map(a => JSON.parse(a))
-
-UsersAndParcels('id', parcels, user);
-
-export default UsersAndParcels;
-*/
