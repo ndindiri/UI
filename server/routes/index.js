43a79e640efd4cfd4591fd8879c64a904c2d937e
ndindiri <ndbut5@gmail.com>
2018-11-23 17:12:52 +0200

    index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,5 @@
 import { Parcel, User } from '../controllers';
-
+ import users from '../datastore/user';
 
 const routes = (app) => {
 	app.get('/api/v1/', (req, res) => {
@@ -7,12 +7,13 @@ const routes = (app) => {
 	});
  
 	app.get('/api/v1/parcels', Parcel.getAllParcels);
+	app.get('/api/v1/parcels/:id', Parcel.getSingleParcel);
 	app.get('/api/v1/users', User.getAllUsers);
 	app.post('/api/v1/parcels', Parcel.addParcel);
     app.put('/api/v1/parcels/:id', Parcel.updateParcel);
     app.delete('/api/v1/parcels/:id', Parcel.deleteParcel);
-    app.get('/api/v1/users/:id/parcels/:id', User.getSingleUser);
-    app.get('/api/v1/users/:id', User.getSingleUser);
+    app.get('/api/v1/users/:id/parcels/:id', User.getParcelById);
+    app.get('/api/v1/users/:id', User.getUserIdUser);
 }
 
 export default routes;
